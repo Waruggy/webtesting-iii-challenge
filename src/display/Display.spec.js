f6f702add5de328f884636c7b22578eccb0719d7
Charles <cwang8413@yahoo.com>
2019-09-17 18:15:05 -0700

    Seems to work now. All tests look passed.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,54 +1,53 @@
 import React from "react";
-import Display from "./Display";
-import renderer from "react-test-renderer";
-import { render, fireEvent } from "@testing-library/react";
+import renderer from "react-test-renderer"; 
+import { render } from "@testing-library/react";
+import 'react-testing-library/cleanup-after-each';
+import Display from './Display'
 
 describe("<Display />", () => {
-  it("matches snapshot", () => {
-    const tree = renderer.create(<Display />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-  it("renders without crashing", () => {
-    render(<Display />);
-  });
-  it("open and unlocked", () => {
-    const { getByText } = render(<Display closed={false} locked={false} />);
-    getByText(/Unlocked/);
-    getByText(/Open/);
-  });
-  it("closed and locked", () => {
-    const { getByText } = render(<Display closed={true} locked={true} />);
-    getByText(/Locked/);
-    getByText(/Closed/);
-  });
-  it("displays Closed if the closed prop is true and Open if otherwise", () => {
-    let { getByText } = render(<Display locked={false} closed={true} />);
-    getByText(/Closed/);
-  });
-  it("displays Closed if the closed prop is true and Open if otherwise", () => {
-    let { getByText } = render(<Display locked={false} closed={false} />);
-    getByText(/Open/);
-  });
-  it("displays Locked if the locked prop is true and Unlocked if otherwise", () => {
-    let { getByText } = render(<Display locked={true} closed={true} />);
-    getByText(/Locked/);
-  });
-  it("displays Locked if the locked prop is true and Unlocked if otherwise", () => {
-    let { getByText } = render(<Display locked={false} closed={true} />);
-    getByText(/Unlocked/);
-  });
-  it("when locked or closed use the red-led class", () => {
-    const { getByText } = render(<Display locked={true} closed={true} />);
-    const closed = getByText(/Closed/);
-    const locked = getByText(/Locked/);
-    expect(closed.classList.contains('red-led')).toBeTruthy();
-    expect(locked.classList.contains('red-led')).toBeTruthy();
-  });
-  it("when unlocked or open use the green-led class", () => {
-    const { getByText } = render(<Display locked={false} closed={false} />);
-    const unlocked = getByText(/Unlocked/);
-    const open = getByText(/Open/);
-    expect(unlocked.classList.contains('green-led')).toBeTruthy();
-    expect(open.classList.contains('green-led')).toBeTruthy();
-  });
-}); 
+    it('renders at all', () => {
+        render(<Display />)
+    });
+})
+
+describe("Display state tests based on props", () => {
+
+    it('shows unlocked/open', () => {
+        const display = render(<Display locked={false} closed={false}/>)
+        expect(display.getByText('Unlocked'))
+        expect(display.getByText('Open'))
+
+        const unlocked = display.getByText('Unlocked');
+        const open = display.getByText('Open');
+        expect(unlocked.className).toMatch('led green-led')
+        expect(open.className).toMatch('led green-led')
+    });
+
+    it('shows unlocked/closed', () => {
+        const display = render(<Display locked={false} closed={true}/>)
+        expect(display.getByText('Unlocked'))
+        expect(display.getByText('Closed'))
+
+        const unlocked = display.getByText('Unlocked');
+        const closed = display.getByText('Closed');
+        expect(unlocked.className).toMatch('led green-led')
+        expect(closed.className).toMatch('led red-led')
+    });
+
+    it('shows locked/closed', () => {
+        const display = render(<Display locked={true} closed={true}/>)
+        expect(display.getByText('Locked'))
+        expect(display.getByText('Closed'))
+
+        const locked = display.getByText('Locked');
+        const closed = display.getByText('Closed');
+        expect(locked.className).toMatch('led red-led')
+        expect(closed.className).toMatch('led red-led')
+    });
+
+    it('DOES NOT ever show locked/open', () => {
+        const display = render(<Display locked={true} closed={true}/>)
+        expect(display.queryByText('Locked')).toBeTruthy()
+        expect(display.queryByText('Open')).toBeFalsy()
+    });
+}) 
